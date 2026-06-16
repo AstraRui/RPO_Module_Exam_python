83eaa9c714c1a7d35d61bb38e6bb31c23ef45882
Astar <asmodeys870@gmail.com>
2026-06-16 08:46:37 +0700

    Обнаружено и исправлено: - Исправлен порт DB (ранее был конфликт с другими приложениями) - Исправлен путь к Log_DIR (ранее из-за указанной системной папки не было доступа для работы с папкой)

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
-DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
